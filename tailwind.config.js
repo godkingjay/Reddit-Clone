98ed1e7c4b5e4cfd3e33b09d88ad25caf3d9465f
Jarrian <glorianagojar@gmail.com>
2023-02-05 13:02:28 +0800

    ADD: custom screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
