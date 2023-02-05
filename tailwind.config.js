a1b167403d5f34e0105111bb216659b20b26e2a4
Jarrian <glorianagojar@gmail.com>
2023-02-05 13:36:46 +0800

    Override to Extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "brand-100": "#ff3c00",
+    extend: {
+      colors: {
+        "brand-100": "#ff3c00",
+      },
+      screens: {
+        xs: "480px",
+      },
     },
-    screens: {
-      xs: "480px",
-      ...defaultTheme.screens,
-    },
-    extend: {},
   },
   plugins: [],
 };
