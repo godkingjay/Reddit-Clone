2c8053386f65da6cade7b363990d6dca4bc70e34
Jarrian <glorianagojar@gmail.com>
2023-02-05 14:39:00 +0800

    add section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,9 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello</h1>
+      <section>
+        <h1>Hello</h1>
+      </section>
     </>
   )
 }
