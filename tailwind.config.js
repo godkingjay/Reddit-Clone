23c79adee5bb3c30cb57a054501e23c45b4adf62
Jarrian <glorianagojar@gmail.com>
2023-02-05 13:23:34 +0800

    move variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "brand-100": "#ff3c00",
+    },
     screens: {
       xs: "480px",
       ...defaultTheme.screens,
