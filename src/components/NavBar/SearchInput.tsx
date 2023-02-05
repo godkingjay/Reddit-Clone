2b1da72eaefbd8484df9d06bb780c7c7e6d0e136
Jarrian <glorianagojar@gmail.com>
2023-02-05 14:40:42 +0800

    Flex Searchbar

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -8,7 +8,7 @@ type SearchInputProps = {
 const SearchInput: React.FC<SearchInputProps> = () => {
   return (
     <div className='flex flex-row flex-1 group'>
-      <div className='flex flex-row justify-center items-center border-gray-400 border-solid border-[1px] rounded-full py-1 px-[16px] space-x-2 group-hover:border-blue-500 group-active:border-blue-500 group-focus-within:border-blue-500'>
+      <div className='flex flex-1 flex-row justify-start items-center border-gray-400 border-solid border-[1px] rounded-full py-1 px-[16px] space-x-2 group-hover:border-blue-500 group-active:border-blue-500 group-focus-within:border-blue-500'>
         <BsSearch
           className='w-[20px] h-full fill-gray-400'
         />
@@ -18,7 +18,7 @@ const SearchInput: React.FC<SearchInputProps> = () => {
           name="search-reddit"
           id="search-reddit"
           placeholder='Search Reddit...'
-          className='bg-transparent outline-none'
+          className='bg-transparent outline-none flex-1'
         />
       </div>
     </div>
