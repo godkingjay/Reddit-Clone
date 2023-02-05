03a905ed17ef53a31cc17cc2690924f1b8a8c667
Jarrian <glorianagojar@gmail.com>
2023-02-05 13:53:09 +0800

    Clean

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,7 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello</h1>
-      </main>
+      <h1>Hello</h1>
     </>
   )
 }
