5afd336eaae3ca1030ed2e7942e63d0b9d596f0e
Jarrian <glorianagojar@gmail.com>
2023-02-06 22:04:14 +0800

    ADD: Auth Inputs

---
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -0,0 +1,28 @@
+import {
+  authModalState
+} from '@/atoms/authModalAtom';
+
+import React from 'react';
+
+import {
+  useRecoilValue
+} from 'recoil';
+
+import Login from './AuthInputs/Login';
+
+type AuthInputsProps = {
+
+};
+
+const AuthInputs: React.FC<AuthInputsProps> = () => {
+  const authModal = useRecoilValue(authModalState);
+
+  return (
+    <div className='flex flex-col items-center w-full'>
+      { authModal.view === 'login' && <Login /> }
+      {/* { authModal.view === 'signup' && <SignUp /> } */}
+    </div>
+  );
+};
+
+export default AuthInputs;
