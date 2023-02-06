fbdd2cd263ce6cfb9c7424f036c96e98a325c90b
Jarrian <glorianagojar@gmail.com>
2023-02-06 21:06:20 +0800

    fix styling

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -21,7 +21,7 @@ const AuthModal: React.FC<AuthModalProps> = () => {
         authModal.open ? (
           <section className='fixed h-screen w-screen max-h-screen max-w-screen bg-[#00000080] top-0 left-0 px-8 py-12 flex justify-center' >
             <div className='absolute -z-10 h-full w-full top-0 left-0 bg-transparent' onClick={() => handleClose()}></div>
-            <div className='relative z-10 flex flex-col bg-white h-min max-h-[640px] max-w-[400px] rounded-[16px] w-full pb-12 pt-16 overflow-y-clip'>
+            <div className='relative z-10 flex flex-col gap-y-4 bg-white h-min max-h-[640px] max-w-[400px] rounded-[16px] w-full pb-12 pt-16 overflow-y-clip'>
               <button
                 type='button'
                 title='Close'
@@ -30,12 +30,17 @@ const AuthModal: React.FC<AuthModalProps> = () => {
               >
                 <BsXLg className='h-full w-full aspect-square' />
               </button>
-              <div className='flex-1 overflow-y-auto px-16'>
-                <h1>
+              <header className='px-16'>
+                <h1 className='font-medium text-xl'>
                   { authModal.view === "login" && "Login" }
                   { authModal.view === "signup" && "Sign Up" }
                   { authModal.view === "resetPassword" && "Reset Password" }
                 </h1>
+              </header>
+              <div className='flex flex-col items-center justify-center flex-1 overflow-y-auto px-16'>
+                {/* <OAuthButtons /> */}
+                {/* <AuthInputs /> */}
+                {/* <ResetPassword /> */}
               </div>
             </div>
           </section> 
