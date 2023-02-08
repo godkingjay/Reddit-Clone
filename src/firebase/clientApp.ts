c13a11f893565442336d4265348bdb4796e980cd
Jarrian <glorianagojar@gmail.com>
2023-02-08 21:12:40 +0800

    initialize firebase

---
+++ src/firebase/clientApp.ts
@@ -0,0 +1,38 @@
+import {
+  initializeApp,
+  getApp,
+  getApps
+} from "firebase/app";
+
+import {
+  getAuth
+} from "firebase/auth";
+
+import {
+  getFirestore
+} from "firebase/firestore";
+
+import {
+  getStorage
+} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export {
+  app,
+  firestore,
+  auth,
+  storage
+};
