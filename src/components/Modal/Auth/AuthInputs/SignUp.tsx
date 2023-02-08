5cd6cb441fb21089f67f46420964ff6617f2c0a7
Jarrian <glorianagojar@gmail.com>
2023-02-08 21:56:51 +0800

    ADD: Loading Spinner when signing up

--- src/components/Modal/Auth/AuthInputs/SignUp.tsx
+++ src/components/Modal/Auth/AuthInputs/SignUp.tsx
@@ -18,6 +18,8 @@ import {
   auth
 } from "@/firebase/clientApp";
 
+import LoadingSpinner from "public/svgs/loading-spinner.svg";
+
 type SignUpProps = {
 
 }
@@ -106,16 +108,28 @@ const SignUp: React.FC<SignUpProps> = () => {
       </div>
       {formError && (
         <>
-          <p className="text-center text-sm text-red-500 my-2">Password does not match.</p>
+          <p className="text-center text-sm text-red-500 mt-2">Password does not match.</p>
         </>
       ) }
-      <button
-        type="submit"
-        title="Sign Up"
-        className="auth-button-modal bg-blue-500 border-blue-500 hover:bg-transparent hover:text-blue-500 focus:bg-transparent focus:text-blue-500"
-      >
-        Sign Up
-      </button>
+      {
+        !loading
+          ? (
+            <button
+              type="submit"
+              title="Sign Up"
+              className="auth-button-modal bg-blue-500 border-blue-500 hover:bg-transparent hover:text-blue-500 focus:bg-transparent focus:text-blue-500"
+            >
+              Sign Up
+            </button>
+          )
+          : (
+            <div className="w-full flex flex-col items-center justify-center mt-6 mb-4 py-2">
+              <LoadingSpinner
+                className="aspect-square h-[32px] w-[32px] animate-spin [&>path]:stroke-blue-500"
+              />
+            </div>
+          )
+      }
       <p className="text-xs text-center">Already a redditor? <button type="button" title="Log In" className="auth-modal-link underline font-bold" tabIndex={0} onClick={() => handleChangeAuth()}>Log In</button></p>
     </form>
   )
