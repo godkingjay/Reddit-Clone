10aec2aacc256a7aecf06ed5b245534bf54d02c6
Jarrian <glorianagojar@gmail.com>
2023-02-09 20:44:23 +0800

    Functional login

--- src/components/Modal/Auth/AuthInputs/Login.tsx
+++ src/components/Modal/Auth/AuthInputs/Login.tsx
@@ -1,8 +1,29 @@
-import { authModalState } from "@/atoms/authModalAtom";
+import {
+  authModalState
+} from "@/atoms/authModalAtom";
+
 import React, {
+  useEffect,
   useState
 } from "react"
-import { useSetRecoilState } from "recoil";
+
+import {
+  useSetRecoilState
+} from "recoil";
+
+import {
+  FIREBASE_ERRORS
+} from "@/firebase/errors";
+
+import {
+  auth
+} from "@/firebase/clientApp";
+
+import {
+  useSignInWithEmailAndPassword
+} from "react-firebase-hooks/auth";
+
+import LoadingSpinner from "public/svg/loading-spinner.svg";
 
 type LoginProps = {}
 
@@ -12,10 +33,28 @@ const Login: React.FC<LoginProps> = () => {
     password: ""
   })
 
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error
+  ] = useSignInWithEmailAndPassword(auth);
+
   const setAuthModal = useSetRecoilState(authModalState);
 
-  const handleSubmit = () => {
+  const [userError, setUserError] = useState<typeof error | null | undefined>(null)
 
+  useEffect(() => {
+    setUserError(error);
+  }, [error])
+  
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(
+      loginForm.email,
+      loginForm.password
+    );
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,7 +81,11 @@ const Login: React.FC<LoginProps> = () => {
           name="email"
           placeholder="Email"
           className="auth-input"
-          onChange={(e) => handleChange(e)}
+          value={ loginForm.email }
+          onChange={(e) => {
+            setUserError(null);
+            handleChange(e);
+          }}
         />
         <input
           required
@@ -51,19 +94,40 @@ const Login: React.FC<LoginProps> = () => {
           name="password"
           placeholder="Password"
           className="auth-input"
-          onChange={(e) => handleChange(e)}
+          value={ loginForm.password }
+          onChange={(e) => {
+            setUserError(null);
+            handleChange(e);
+          }}
         />
       </div>
       {/* <p className="text-left text-xs mt-4 mb-2">Forgot your username or password?</p> */}
       <div className="w-full mb-4 mt-6 flex flex-col">
-        <button
-          type="submit"
-          title="Login"
-          className="auth-button-modal bg-brand-100 border-brand-100 hover:bg-transparent hover:text-brand-100 focus:bg-transparent focus:text-brand-100"
-        >
-          Login
-        </button>
+        {
+          !loading
+          ? (
+            <button
+              type="submit"
+              title="Login"
+              className="auth-button-modal bg-brand-100 border-brand-100 hover:bg-transparent hover:text-brand-100 focus:bg-transparent focus:text-brand-100"
+            >
+              Login
+            </button>
+          )
+          : (
+            <div className="w-full h-max flex flex-col items-center justify-center my-1">
+              <LoadingSpinner
+                className="aspect-square h-[32px] w-[32px] animate-spin [&>path]:stroke-blue-500"
+              />
+            </div>
+          )
+        }
       </div>
+      {error && userError && (
+        <p className="w-full break-words text-sm text-center text-red-500 mb-4">
+          { error && FIREBASE_ERRORS[userError.message as keyof typeof FIREBASE_ERRORS] ? FIREBASE_ERRORS[userError.message as keyof typeof FIREBASE_ERRORS] : userError.message }
+        </p>
+      )}
       <p className="text-center text-xs">New to Reddit? <button type="button" title="Sign Up" className="auth-modal-link font-bold underline" tabIndex={0} onClick={() => handleChangeAuth()}>Sign Up</button></p>
     </form>
   )
--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,3 +1,5 @@
 export const FIREBASE_ERRORS = {
-  "Firebase: Error (auth/email-already-in-use).": "A user with that email already exists."
+  "Firebase: Error (auth/email-already-in-use).": "A user with that email already exists.",
+  "Firebase: Error (auth/user-not-found).": "Incorrect username or password.",
+  "Firebase: Error (auth/wrong-password).": "Incorrect username or password."
 }
