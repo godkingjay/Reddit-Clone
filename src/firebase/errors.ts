b03ff332c41c7714f9288ff70d19d7ad37620985
Jarrian <glorianagojar@gmail.com>
2023-02-09 21:01:12 +0800

    add network error request

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,5 +1,6 @@
 export const FIREBASE_ERRORS = {
   "Firebase: Error (auth/email-already-in-use).": "A user with that email already exists.",
   "Firebase: Error (auth/user-not-found).": "Incorrect username or password.",
-  "Firebase: Error (auth/wrong-password).": "Incorrect username or password."
+  "Firebase: Error (auth/wrong-password).": "Incorrect username or password.",
+  "Firebase: Error (auth/network-request-failed)": "Network request failed. Please try again later.",
 }
