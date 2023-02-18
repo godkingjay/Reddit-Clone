ddd61d395f88ce11793a7aa32cf7e387018c34a1
Jarrian <glorianagojar@gmail.com>
2023-02-18 13:05:36 +0800

    style: center

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -8,7 +8,7 @@ type SearchInputProps = {
 
 const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
 	return (
-		<div className="flex flex-row flex-1 group max-w-[600px] h-full">
+		<div className="flex flex-row flex-1 group max-w-[600px] h-full mx-auto">
 			<div className="flex flex-1 flex-row justify-start items-center bg-gray-100 border-transparent border-solid border-[1px] rounded-full py-1 px-[16px] space-x-2 group-hover:border-blue-500 group-active:border-blue-500 group-focus-within:border-blue-500">
 				<BsSearch className="w-[20px] h-full fill-gray-400" />
 				<input
