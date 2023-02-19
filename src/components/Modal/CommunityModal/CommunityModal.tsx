61169ab7f23bf0d26dcf4f6dc3b35bf15dc019bd
Jarrian <glorianagojar@gmail.com>
2023-02-19 16:06:32 +0800

    fix: modal overflow

--- src/components/Modal/CommunityModal/CommunityModal.tsx
+++ src/components/Modal/CommunityModal/CommunityModal.tsx
@@ -20,12 +20,12 @@ const CommunityModal: React.FC<CommunityModalProps> = () => {
 	return (
 		<>
 			{communityModal.open ? (
-				<section className="modal-pop-up z-50 fixed h-screen w-screen max-h-screen max-w-screen bg-[#00000080] top-0 left-0 px-8 py-12 flex flex-col items-center">
+				<section className="modal-pop-up z-50 fixed h-screen w-screen min-h-screen max-w-screen bg-[#00000080] top-0 left-0 px-8 py-12 overflow-y-auto scroll-y-style flex flex-col items-center">
 					<div
 						className="absolute -z-10 h-full w-full top-0 left-0 bg-transparent"
 						onClick={() => handleClose()}
 					></div>
-					<div className="relative z-10 flex flex-col gap-y-4 bg-white h-min max-h-[640px] overflow-hidden max-w-[720px] rounded-[16px] w-full items-center">
+					<div className="relative z-10 flex flex-col gap-y-4 bg-white h-min max-h-[640px] max-w-[720px] rounded-[16px] w-full items-center">
 						<div className="w-full flex flex-row items-center justify-between border-b-[1px] py-2 px-4">
 							<h1 className="font-bold text-lg text-gray-700">
 								{communityModal.view === "create" && "Create Community"}
