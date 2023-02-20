6011437b609b4c02a91974b12afa3ce4d035f837
Jarrian <glorianagojar@gmail.com>
2023-02-20 19:36:37 +0800

    feat: add community creation

--- src/components/Modal/CommunityModal/CommunityModal.tsx
+++ src/components/Modal/CommunityModal/CommunityModal.tsx
@@ -25,7 +25,7 @@ const CommunityModal: React.FC<CommunityModalProps> = () => {
 						className="absolute -z-10 h-full w-full top-0 left-0 bg-transparent"
 						onClick={() => handleClose()}
 					></div>
-					<div className="relative z-10 flex flex-col gap-y-4 bg-white h-min max-h-[640px] max-w-[720px] rounded-[16px] w-full items-center">
+					<div className="relative z-10 flex flex-col bg-white h-min max-h-[640px] max-w-[720px] rounded-[16px] w-full items-center">
 						<div className="w-full flex flex-row items-center justify-between border-b-[1px] py-[12px] px-4">
 							<h1 className="font-bold text-xl text-gray-700">
 								{communityModal.view === "create" && "Create Community"}
@@ -39,8 +39,8 @@ const CommunityModal: React.FC<CommunityModalProps> = () => {
 								<BsXLg className="icon h-full w-full aspect-square" />
 							</button>
 						</div>
-						<div className="relative flex flex-1 flex-col overflow-hidden items-center w-full pb-2">
-							<div className="relative flex flex-col flex-1 items-center w-full overflow-y-auto overflow-x-hidden scroll-y-style px-4">
+						<div className="relative flex flex-1 flex-col overflow-hidden items-center w-full pt-4 pb-2">
+							<div className="flex flex-col flex-1 items-center w-full overflow-y-auto overflow-x-hidden scroll-y-style px-4">
 								{communityModal.view === "create" && (
 									<CreateCommunity handleClose={handleClose} />
 								)}
--- src/components/Modal/CommunityModal/Inputs/Create.tsx
+++ src/components/Modal/CommunityModal/Inputs/Create.tsx
@@ -1,22 +1,27 @@
 import { firestore } from "@/firebase/clientApp";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { FaUserAlt } from "react-icons/fa";
 import { HiLockClosed } from "react-icons/hi";
 import { IoAlertCircleOutline } from "react-icons/io5";
 import { RiEyeCloseFill } from "react-icons/ri";
+import { auth } from "@/firebase/clientApp";
+import LoadingSpinner from "public/svg/loading-spinner.svg";
 
 type CreateProps = {
 	handleClose: Function;
 };
 
 const Create: React.FC<CreateProps> = ({ handleClose }) => {
+	const [user] = useAuthState(auth);
 	const [createCommunityForm, setCreateCommunityForm] = useState({
 		communityName: "",
-		communityType: "public",
+		privacyType: "public",
 	});
 	const [communityNameLength, setCommunityNameLength] = useState(0);
 	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setCreateCommunityForm((prev) => ({
@@ -51,137 +56,176 @@ const Create: React.FC<CreateProps> = ({ handleClose }) => {
 
 	const handleCreateCommunity = async (e: React.FormEvent) => {
 		e.preventDefault();
-		// if (error.length == 0 && communityNameLength > 2) {
-		// 	const communityDocRef = doc(
-		// 		firestore,
-		// 		"communities",
-		// 		createCommunityForm.communityName
-		// 	);
-		// 	const communityDoc = await getDoc(communityDocRef);
-		// } else {
-		// 	return;
-		// }
+
+		if (error.length != 0 || communityNameLength < 3) return;
+
+		setError("");
+		setLoading(true);
+
+		// ! try and catch block for community creation.
+		try {
+			const communityDocRef = doc(
+				firestore,
+				"communities",
+				createCommunityForm.communityName
+			);
+			const communityDoc = await getDoc(communityDocRef);
+
+			// ! Check if community exists
+			if (communityDoc.exists()) {
+				throw new Error(
+					`Sorry, r/${createCommunityForm.communityName} is taken. Try another.`
+				);
+			}
+
+			// ! Create community
+			await setDoc(communityDocRef, {
+				creatorId: user?.uid,
+				createdAt: serverTimestamp(),
+				members: 1,
+				privacyType: createCommunityForm.privacyType,
+			});
+		} catch (error: any) {
+			console.log("Error creating community: ", error);
+			setError(error.message as string);
+		}
+
+		setLoading(false);
 	};
 
 	return (
-		<form
-			className="full flex flex-col gap-y-4"
-			onSubmit={handleCreateCommunity}
-		>
-			<div className="flex flex-col gap-y-2">
-				<div className="flex flex-col w-full">
-					<h2 className="font-semibold text-lg">Name</h2>
-					<div className="flex flex-row items-center gap-x-2">
-						<p className="text-xs text-gray-500">
-							Community names including capitalization cannot be changed.
-						</p>
-						<div className="create-community-alert relative h-max w-max">
-							<IoAlertCircleOutline className="icon aspect-square w-[16px] h-[16px] scale-125 text-gray-500" />
+		<>
+			<form
+				className="full flex flex-col gap-y-4"
+				onSubmit={handleCreateCommunity}
+			>
+				<div className="flex flex-col gap-y-2">
+					<div className="flex flex-col w-full">
+						<h2 className="font-semibold text-lg">Name</h2>
+						<div className="flex flex-row items-center gap-x-2">
+							<p className="text-xs text-gray-500">
+								Community names including capitalization cannot be changed.
+							</p>
+							<div className="create-community-alert relative h-max w-max">
+								<IoAlertCircleOutline className="icon aspect-square w-[16px] h-[16px] scale-125 text-gray-500" />
+							</div>
+						</div>
+					</div>
+					<div className="w-full flex flex-col gap-y-2">
+						<div className="flex flex-row items-center border-[1px] border-gray-300 border-solid px-2 py-2 rounded-md text-base mt-2 focus-within:border-blue-500">
+							<p className="text-gray-500">r/</p>
+							<input
+								required
+								type="text"
+								title="Community Name"
+								className="flex-1 min-w-0 outline-none bg-transparent"
+								name="communityName"
+								onChange={handleChange}
+								maxLength={21}
+							/>
+						</div>
+						<div className="text-xs">
+							<p
+								className={
+									communityNameLength === 21 ? "text-red-500" : "text-gray-500"
+								}
+							>
+								{21 - communityNameLength} Characters remaining
+							</p>
+							{createCommunityForm.communityName.length === 0 && (
+								<p className="text-red-500">A community name is required</p>
+							)}
+							{error.length > 1 && <p className="text-red-500">{error}</p>}
 						</div>
 					</div>
 				</div>
-				<div className="w-full flex flex-col gap-y-2">
-					<div className="flex flex-row items-center border-[1px] border-gray-300 border-solid px-2 py-2 rounded-md text-base mt-2 focus-within:border-blue-500">
-						<p className="text-gray-500">r/</p>
-						<input
-							required
-							type="text"
-							title="Community Name"
-							className="flex-1 min-w-0 outline-none bg-transparent"
-							name="communityName"
-							onChange={handleChange}
-							maxLength={21}
-						/>
+				<div>
+					<div className="flex flex-col w-full">
+						<h2 className="font-semibold text-lg">Community Type</h2>
 					</div>
-					<div className="text-xs">
-						<p
-							className={
-								communityNameLength === 21 ? "text-red-500" : "text-gray-500"
-							}
-						>
-							{21 - communityNameLength} Characters remaining
-						</p>
-						{createCommunityForm.communityName.length === 0 && (
-							<p className="text-red-500">A community name is required</p>
-						)}
-						{error.length > 1 && <p className="text-red-500">{error}</p>}
-					</div>
-				</div>
-			</div>
-			<div>
-				<div className="flex flex-col w-full">
-					<h2 className="font-semibold text-lg">Community Type</h2>
-				</div>
-				<div className="create-community-radios">
-					<div>
-						<input
-							type="radio"
-							name="communityType"
-							title="Public"
-							id="community-type-public"
-							defaultChecked={true}
-							onChange={handleSelect}
-						/>
-						<label htmlFor="community-type-public">
-							<FaUserAlt className="icon" />
-							<p>Public</p>
-							<small>
-								Anyone can view, post, and comment to this community
-							</small>
-						</label>
-					</div>
-					<div>
-						<input
-							type="radio"
-							name="communityType"
-							title="Restricted"
-							id="community-type-restricted"
-							onChange={handleSelect}
-						/>
-						<label htmlFor="community-type-restricted">
-							<RiEyeCloseFill className="icon" />
-							<p>Restricted</p>
-							<small>
-								Anyone can view this community, but only approved users can post
-							</small>
-						</label>
-					</div>
-					<div>
-						<input
-							type="radio"
-							name="communityType"
-							title="Private"
-							id="community-type-private"
-							onChange={handleSelect}
-						/>
-						<label htmlFor="community-type-private">
-							<HiLockClosed className="icon" />
-							<p>Private</p>
-							<small>
-								Only approved users can view and submit to this community
-							</small>
-						</label>
+					<div className="create-community-radios">
+						<div>
+							<input
+								type="radio"
+								name="privacyType"
+								title="Public"
+								id="community-type-public"
+								defaultChecked={true}
+								onChange={handleSelect}
+							/>
+							<label htmlFor="community-type-public">
+								<FaUserAlt className="icon" />
+								<p>Public</p>
+								<small>
+									Anyone can view, post, and comment to this community
+								</small>
+							</label>
+						</div>
+						<div>
+							<input
+								type="radio"
+								name="privacyType"
+								title="Restricted"
+								id="community-type-restricted"
+								onChange={handleSelect}
+							/>
+							<label htmlFor="community-type-restricted">
+								<RiEyeCloseFill className="icon" />
+								<p>Restricted</p>
+								<small>
+									Anyone can view this community, but only approved users can
+									post
+								</small>
+							</label>
+						</div>
+						<div>
+							<input
+								type="radio"
+								name="privacyType"
+								title="Private"
+								id="community-type-private"
+								onChange={handleSelect}
+							/>
+							<label htmlFor="community-type-private">
+								<HiLockClosed className="icon" />
+								<p>Private</p>
+								<small>
+									Only approved users can view and submit to this community
+								</small>
+							</label>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="flex flex-row items-center justify-end gap-x-4 max-[320px]:flex-col-reverse max-[320px]:gap-y-2 max-[320px]:[&>button]:w-full">
-				<button
-					type="button"
-					title="Cancel"
-					className="auth-button-modal text-sm bg-transparent border-red-500 px-6 text-red-500 hover:bg-red-500 hover:text-white focus:bg-red-500 focus:text-white"
-					onClick={() => handleClose()}
-				>
-					Cancel
-				</button>
-				<button
-					type="submit"
-					title="Create Community"
-					className="auth-button-modal text-sm px-6 hover:bg-blue-600 hover:border-blue-600 focus:bg-blue-600 focus:border-blue-600"
-				>
-					Create<span className="hidden xs:inline"> Community</span>
-				</button>
-			</div>
-		</form>
+				<div className="flex flex-row items-center justify-end gap-x-4 max-[320px]:flex-col-reverse max-[320px]:gap-y-2 max-[320px]:[&>button]:w-full">
+					<button
+						type="button"
+						title="Cancel"
+						className="auth-button-modal text-sm bg-transparent border-red-500 px-6 text-red-500 hover:bg-red-500 hover:text-white focus:bg-red-500 focus:text-white"
+						onClick={() => handleClose()}
+						disabled={loading}
+					>
+						Cancel
+					</button>
+					<button
+						type="submit"
+						title="Create Community"
+						className="auth-button-modal text-sm px-6 hover:bg-blue-600 hover:border-blue-600 focus:bg-blue-600 focus:border-blue-600"
+					>
+						Create<span className="hidden xs:inline"> Community</span>
+					</button>
+				</div>
+			</form>
+			{loading && (
+				<div className="modal-loading-box">
+					<div className="loading-content">
+						<div className="loading-container">
+							<LoadingSpinner className="loading" />
+						</div>
+						<p className="label">Creating Community</p>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
