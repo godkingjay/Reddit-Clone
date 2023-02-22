769c0c5c107b834247b4a381f10586f7119ecdba
Jarrian <glorianagojar@gmail.com>
2023-02-22 19:52:06 +0800

    feat: add link to logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,27 +5,31 @@ import RightContent from "./RightContent/RightContent";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/clientApp";
 import Directory from "./Directory/Directory";
+import Link from "next/link";
 
 const NavBar: React.FC = () => {
 	const [user, loading, error] = useAuthState(auth);
 
 	return (
 		<div className="flex bg-hsl(0, 0%, 100%) px-[16px] py-[8px] gap-x-2 items-center">
-			<div className="flex">
+			<Link
+				href={"/"}
+				className="flex"
+			>
 				<Image
 					src={"/images/redditFace.svg"}
 					alt="Reddit Face"
-					height={38}
-					width={38}
+					height={36}
+					width={36}
 				/>
 				<Image
 					src={"/images/redditText.svg"}
 					alt="Reddit Text"
-					height={38}
+					height={36}
 					width={56}
 					className="hidden sm:block"
 				/>
-			</div>
+			</Link>
 			<div className="flex flex-row h-full flex-1 items-center justify-between gap-x-2">
 				{user && <Directory />}
 				<SearchInput user={user} />
