49ac0b21dfcf72242619e0c3fb2a4de1c411ae07
Jarrian <glorianagojar@gmail.com>
2023-02-23 10:48:46 +0800

    chore: add no community image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		domains: [],
+	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/i,
--- src/components/CommunityPage/Header.tsx
+++ src/components/CommunityPage/Header.tsx
@@ -1,9 +1,11 @@
+import { Community } from "@/atoms/communitiesAtom";
+import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
+import NoCommunityImage from "public/svg/community-no-image.svg";
 
 type HeaderProps = {
-	communityData: any;
+	communityData: Community;
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
@@ -15,16 +17,29 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
 					target="_blank"
 					className="w-full h-max"
 				>
-					<div className="w-full h-24 bg-blue-500"></div>
+					<div className="w-full h-24 bg-blue-400"></div>
 				</Link>
 			) : (
 				<div className="w-full h-max">
-					<div className="w-full h-24 bg-blue-500"></div>
+					<div className="w-full h-24 bg-blue-400"></div>
 				</div>
 			)}
 			<div className="max-w-6xl w-full flex flex-col px-6">
 				<div className="relative w-full flex flex-row gap-x-4">
-					<div className="border-4 border-solid border-white aspect-square w-24 h-24 rounded-full bg-blue-500 translate-y-[-25%]"></div>
+					<div className="border-4 border-solid border-white aspect-square w-24 h-24 rounded-full bg-white translate-y-[-25%]">
+						{communityData.imageURL ? (
+							<Image
+								src={communityData.imageURL}
+								alt={`${communityData.name} image`}
+								width={256}
+								height={256}
+								loading="lazy"
+								className="w-full h-full rounded-full bg-contain bg-center"
+							/>
+						) : (
+							<NoCommunityImage className="w-full h-full rounder-full fill-blue-600" />
+						)}
+					</div>
 					<div className="my-2 flex flex-col gap-y-1 flex-1">
 						<h1 className="text-2xl font-bold break-words">
 							{communityData.name}
