f36815d2bb62cc5a287056e0cf0ef0d2a0c55f10
Jarrian <glorianagojar@gmail.com>
2023-02-24 13:06:22 +0800

    chore: remove unused

--- src/components/Modal/CommunityModal/Inputs/Create.tsx
+++ src/components/Modal/CommunityModal/Inputs/Create.tsx
@@ -4,7 +4,6 @@ import {
 	getDoc,
 	runTransaction,
 	serverTimestamp,
-	setDoc,
 } from "firebase/firestore";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
