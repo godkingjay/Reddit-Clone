a228f226ccf1e55828aa7f40773ae2953b27bec8
Jarrian <glorianagojar@gmail.com>
2023-02-25 15:02:56 +0800

    feat: new post header

--- src/components/Posts/NewPostHeader.tsx
+++ src/components/Posts/NewPostHeader.tsx
@@ -2,8 +2,8 @@ type NewPostHeaderProps = {};
 
 const NewPostHeader: React.FC<NewPostHeaderProps> = () => {
 	return (
-		<div className="bordered-box-1 bg-white px-2 py-2 rounded-md">
-			<h1 className="font-semibold text-lg text-gray-800">Create a Post</h1>
+		<div className="bordered-box-1 bg-white px-3 py-3 rounded-md">
+			<h1 className="font-semibold text-xl text-gray-800">Create a Post</h1>
 		</div>
 	);
 };
