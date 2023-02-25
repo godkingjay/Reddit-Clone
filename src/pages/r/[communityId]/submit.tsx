145e8235fb3da339eeb58a530a2f3a3f7b0a6f56
Jarrian <glorianagojar@gmail.com>
2023-02-25 14:10:58 +0800

    style: bordered box style

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,25 @@
-type SubmitPostProps = {};
+import Sidebar from "@/components/CommunityPage/Sidebar";
+import PageContentLayout from "@/components/Layout/PageContentLayout";
+import NewPostForm from "@/components/Posts/NewPostForm";
+import NewPostHeader from "@/components/Posts/NewPostHeader";
 
-const SubmitPost: React.FC<SubmitPostProps> = () => {
-	return <div>Submit Post</div>;
+type SubmitPostPageProps = {};
+
+const SubmitPostPage: React.FC<SubmitPostPageProps> = () => {
+	return (
+		<PageContentLayout>
+			<>
+				<div className="w-full flex flex-col gap-y-4">
+					<NewPostHeader />
+					<NewPostForm />
+				</div>
+			</>
+			<>
+				{/* <Sidebar communityData={communityData} /> */}
+				<div>Hello</div>
+			</>
+		</PageContentLayout>
+	);
 };
 
-export default SubmitPost;
+export default SubmitPostPage;
