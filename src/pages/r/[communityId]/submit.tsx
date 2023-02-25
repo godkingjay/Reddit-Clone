f6381e4d02743b61cab92cf36608e902c9efa08e
Jarrian <glorianagojar@gmail.com>
2023-02-25 15:01:16 +0800

    style: center page

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -2,23 +2,31 @@ import Sidebar from "@/components/CommunityPage/Sidebar";
 import PageContentLayout from "@/components/Layout/PageContentLayout";
 import NewPostForm from "@/components/Posts/NewPostForm";
 import NewPostHeader from "@/components/Posts/NewPostHeader";
+import Head from "next/head";
 
 type SubmitPostPageProps = {};
 
 const SubmitPostPage: React.FC<SubmitPostPageProps> = () => {
 	return (
-		<PageContentLayout>
-			<>
-				<div className="w-full flex flex-col gap-y-4">
-					<NewPostHeader />
-					<NewPostForm />
-				</div>
-			</>
-			<>
-				{/* <Sidebar communityData={communityData} /> */}
-				<div>Hello</div>
-			</>
-		</PageContentLayout>
+		<>
+			<Head>
+				<title>Create a Post</title>
+			</Head>
+			<section className="flex flex-col items-center">
+				<PageContentLayout>
+					<>
+						<div className="w-full flex flex-col gap-y-4">
+							<NewPostHeader />
+							<NewPostForm />
+						</div>
+					</>
+					<>
+						{/* <Sidebar communityData={communityData} /> */}
+						<div>Hello</div>
+					</>
+				</PageContentLayout>
+			</section>
+		</>
 	);
 };
 
