cc84f42d21d64fa6f1f4f546216f212d47d2471b
Jarrian <glorianagojar@gmail.com>
2023-02-25 16:08:14 +0800

    feat: post title input field

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -2,7 +2,7 @@ import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import { BiPoll } from "react-icons/bi";
 import TabItem from "./TabItem";
-import { useState } from "react";
+import React, { useState } from "react";
 
 type NewPostFormProps = {};
 
@@ -36,6 +36,31 @@ const formTabs: FormTabItem[] = [
 
 const NewPostForm: React.FC<NewPostFormProps> = () => {
 	const [currentTab, setCurrentTab] = useState(formTabs[0].title);
+	const [postInput, setPostInput] = useState({
+		title: "",
+		body: "",
+	});
+	const [selectedFile, setSelectedFile] = useState<string>();
+	const [postInputLength, setPostInputLength] = useState({
+		title: 0,
+	});
+
+	const handleCreatePost = () => {};
+
+	const handleSelectImage = () => {};
+
+	const handleTextChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setPostInput((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+		setPostInputLength((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value.length,
+		}));
+	};
 
 	return (
 		<div className="bordered-box-1 flex flex-col bg-white rounded-md overflow-hidden">
@@ -49,7 +74,32 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
 					/>
 				))}
 			</div>
-			<div>Post</div>
+			<div className="p-4 flex flex-col">
+				<div className="flex flex-row border-[1px] border-solid border-gray-300 py-2 px-4 rounded-md hover:border-blue-500 focus-within:border-blue-500 gap-x-2">
+					<textarea
+						name="title"
+						title="Post Title"
+						placeholder="Title"
+						className="flex-1 min-w-0 outline-none text-sm bg-transparent font-semibold break-words resize-none"
+						maxLength={300}
+						onChange={(e) => {
+							handleTextChange(e);
+							e.currentTarget.style.height = "0px";
+							e.currentTarget.style.height =
+								e.currentTarget.scrollHeight + "px";
+						}}
+						rows={1}
+					/>
+					<p
+						className={`mt-auto text-2xs font-semibold
+          ${
+						300 - postInputLength.title === 0 ? "text-red-500" : "text-gray-400"
+					}`}
+					>
+						{300 - postInputLength.title}/300
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
