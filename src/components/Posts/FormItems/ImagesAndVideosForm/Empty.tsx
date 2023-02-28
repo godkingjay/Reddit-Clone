8635d09fdbb3c12bb58655d3bf59b17efdfb78c3
Jarrian <glorianagojar@gmail.com>
2023-02-28 16:26:36 +0800

    feat: remove nested buttons

--- src/components/Posts/FormItems/ImagesAndVideosForm/Empty.tsx
+++ src/components/Posts/FormItems/ImagesAndVideosForm/Empty.tsx
@@ -7,13 +7,7 @@ type EmptyProps = {
 
 const Empty: React.FC<EmptyProps> = ({ uploadImagesAndVideos, loading }) => {
 	return (
-		<button
-			type="button"
-			title="Upload"
-			className="relative flex flex-col border-2 bg-transparent border-dashed border-gray-300 rounded-md hover:border-blue-500 focus-within:border-blue-500 gap-x-2 outline-none"
-			onClick={() => uploadImagesAndVideos.current?.click()}
-			disabled={loading}
-		>
+		<div className="relative flex flex-col border-2 bg-transparent border-dashed border-gray-300 rounded-md hover:border-blue-500 focus-within:border-blue-500 gap-x-2 outline-none">
 			<div className="h-[240px] w-full flex flex-col items-center justify-center">
 				<div className="w-full max-w-[128px]">
 					<button
@@ -27,7 +21,7 @@ const Empty: React.FC<EmptyProps> = ({ uploadImagesAndVideos, loading }) => {
 					</button>
 				</div>
 			</div>
-		</button>
+		</div>
 	);
 };
 
