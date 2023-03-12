60faaf8f4c282d537e4156b555618271da6342f5
Jarrian <glorianagojar@gmail.com>
2023-03-12 10:52:27 +0800

    feat: Add user-specific home feed and post votes feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,17 @@ import { firestore } from "@/firebase/clientApp";
 import useAuth from "@/hooks/useAuth";
 import useCommunityData from "@/hooks/useCommunityData";
 import usePosts from "@/hooks/usePosts";
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import {
+	collection,
+	getDocs,
+	limit,
+	orderBy,
+	query,
+	where,
+} from "firebase/firestore";
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import { Post } from "@/atoms/postAtom";
+import { Post, PostVote } from "@/atoms/postAtom";
 import PostSkeleton from "@/components/Skeletons/PostSkeleton";
 import PostItem from "@/components/Posts/PostItem";
 import CreatePostLink from "@/components/CommunityPage/CreatePostLink";
@@ -23,7 +30,38 @@ const Home = () => {
 	} = usePosts();
 	const [loading, setLoading] = useState(false);
 
-	const buildUserHomeFeed = () => {};
+	const buildUserHomeFeed = async () => {
+		setLoading(true);
+		try {
+			if (communityStateValue.userCommunities.length > 0) {
+				const userCommunityIds = communityStateValue.userCommunities.map(
+					(community) => community.communityId
+				);
+
+				const postQuery = query(
+					collection(firestore, "posts"),
+					where("communityId", "in", userCommunityIds),
+					limit(10)
+				);
+
+				const postDocs = await getDocs(postQuery);
+				const posts = postDocs.docs.map((doc) => ({
+					...doc.data(),
+					id: doc.id,
+				}));
+
+				setPostStateValue((prev) => ({
+					...prev,
+					posts: posts as Post[],
+				}));
+			} else {
+				buildNoUserHomeFeed();
+			}
+		} catch (error: any) {
+			console.log("Fetching user home feed error: ", error.message);
+		}
+		setLoading(false);
+	};
 
 	const buildNoUserHomeFeed = async () => {
 		setLoading(true);
@@ -52,7 +90,31 @@ const Home = () => {
 		setLoading(false);
 	};
 
-	const getUserPostVotes = () => {};
+	const getUserPostVotes = async () => {
+		setLoading(true);
+		try {
+			const postVotesQuery = query(
+				collection(firestore, `users/${user?.uid}/postVotes`),
+				limit(10)
+			);
+
+			const postVotesDocs = await getDocs(postVotesQuery);
+			const postVotes = postVotesDocs.docs.map((doc) => {
+				return {
+					...doc.data(),
+					id: doc.id,
+				};
+			});
+
+			setPostStateValue((prev) => ({
+				...prev,
+				postVotes: postVotes as PostVote[],
+			}));
+		} catch (error: any) {
+			console.log("Fetching user post votes error: ", error.message);
+		}
+		setLoading(false);
+	};
 
 	useEffect(() => {
 		if (user && !loadingUser) {
@@ -61,6 +123,12 @@ const Home = () => {
 			buildNoUserHomeFeed();
 		}
 	}, [user, loadingUser]);
+
+	useEffect(() => {
+		if (user && !loadingUser) {
+			getUserPostVotes();
+		}
+	}, [postStateValue.posts]);
 
 	return (
 		<>
@@ -71,7 +139,7 @@ const Home = () => {
 				<PageContentLayout>
 					<>
 						<div className="flex flex-col w-full gap-y-4">
-							{!loading ? (
+							{!loading && !loadingUser ? (
 								<>
 									<CreatePostLink user={user} />
 									{postStateValue.posts.map((post: Post) => {
